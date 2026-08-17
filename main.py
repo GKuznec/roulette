524cf73f08fb2f0ffd83cc51af3ba5d1cdb916de
Oleksii <alexeysytnik123@gmai.com>
2026-08-17 17:41:51 +0200

    Added the option to bet on colors and a win/loss message.

--- main.py
+++ main.py
@@ -157,7 +157,6 @@ while running:
                 res_text = res.render('You won!',True,color_tuple[spin_result])
             else:
                 res_text = res.render('You lost!',True,(0,0,255))
-
     if game_over:
         res_text_rect = res_text.get_rect(center=(500, 50))
         screen.blit(res_text,res_text_rect)
